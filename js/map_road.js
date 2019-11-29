734854ca9f5077c98a613ca4039cc5943ed4a4a0
Wooyoung <souljit2@gmail.com>
2019-11-29 18:01:55 +0900

    Kusitms-Academic : road making

--- js/map_road.js
+++ js/map_road.js
@@ -1,6 +1,35 @@
-var container = document.getElementById('map')
+var container = document.getElementById("map");
+
 var options = {
-    center: new kakao.maps.LatLng(33.450701, 126.570667),
-    level: 3
-}
-var map = new kakao.maps.Map(container, options)
+  center: new kakao.maps.LatLng(37.554830695156097, 126.92956606679954),
+  level: 5
+};
+var map = new kakao.maps.Map(container, options);
+
+var markers;
+var clusterer = new kakao.maps.MarkerClusterer({
+  map: map,
+  markers: markers,
+  gridSize: 35,
+  averageCenter: true,
+  minLevel: 6,
+  disableClickZoom: true,
+  styles: [
+    {
+      width: "53px",
+      height: "52px",
+      background: "url(cluster.png) no-repeat",
+      color: "#fff",
+      textAlign: "center",
+      lineHeight: "54px"
+    }
+  ]
+});
+marker = new kakao.maps.Marker({
+  position: new kakao.maps.LatLng(37.55422131492838, 126.92301169467778)
+});
+clusterer.addMarker(marker);
+marker = new kakao.maps.Marker({
+  position: new kakao.maps.LatLng(37.555440075383814, 126.9361204389213)
+});
+clusterer.addMarker(marker);
